771283dcbef6204630f3eb85d3d93c3ff6cecbcd
caffeine <eniehack@outlook.jp>
2018-11-04 22:17:56 +0900

    Create User Model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,77 @@
 from django.db import models
+from django.core.mail import send_mail
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.utils.translation import ugettext_lazy as _
+import uuid
+from django.utils import timezone
+from django.contrib.auth.base_user import BaseUserManager
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    user_id = models.UUIDField(_('user id'), default=uuid.uuid4, editable=False, unique=True, primary_key=True)
+    email = models.EmailField(_('email address'), unique=True)
+    user_name = models.CharField(_('user name'), max_length=150)
+    twitter = models.CharField('Twitter', max_length=15, null=True, blank=True)
+    homepage = models.URLField('homepage', null=True, blank=True)
+
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+    )
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+    )
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    objects = UserManager()
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'user_id'
+    REQUIRED_FIELDS = ['email','user_name']
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def get_full_name(self):
+        return self.user_name
+
+    def get_short_name(self):
+        return self.user_name
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        send_mail(subject, message, from_email, [self.email], **kwargs)
