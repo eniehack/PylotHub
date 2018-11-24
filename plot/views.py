dedd7e2dab76f8172b0e5f67ad5a66c72fcacdf4
caffeine <eniehack@outlook.jp>
2018-11-24 17:51:22 +0900

    designed plot app's view

--- plot/views.py
+++ plot/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from django.views.generic import (
+    DetailView,
+    CreateView, 
+    UpdateView, 
+    DeleteView,
+    TemplateView,
+    ListView
+)
 
-# Create your views here.
+
+class Index(TemplateView):
+    pass
+
+index = Index.as_view()
+
+class ViewPlot(DetailView):
+    pass
+
+view_plot = ViewPlot.as_view()
+
+class CreatePlot(CreateView):
+    pass
+
+create_plot = CreatePlot.as_view()
+
+class EditPlot(UpdateView):
+    pass
+
+edit_plot = EditPlot.as_view()
+
+class DeletePlot(DeleteView):
+    pass
+
+delete_plot = DeletePlot.as_view()
+
+class SearchPlot(ListView):
+    
+    model = Plot
+
+search_plot = SearchPlot.as_view()
