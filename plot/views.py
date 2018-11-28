ce9499e5fe3929ed1f2079dc3821799dc5f6c65d
caffeine <eniehack@outlook.jp>
2018-11-28 14:13:58 +0900

    got better create plots.

--- plot/views.py
+++ plot/views.py
@@ -1,35 +1,49 @@
-from django.shortcuts import render
-from .models import Plot
 from django.views.generic import \
     DetailView, CreateView, UpdateView, DeleteView, TemplateView, ListView
+
+from .models import Plot
+from .forms import CreateForm
 
 
 class Index(TemplateView):
     template_name = 'plot/index.html'
 
+
 index = Index.as_view()
+
 
 class ViewPlot(DetailView):
     pass
 
 view_plot = ViewPlot.as_view()
 
+
 class CreatePlot(CreateView):
-    pass
+
+    model = Plot
+    form_class = CreateForm
+    template_name = 'plot/create.html'
+
 
 create_plot = CreatePlot.as_view()
+
 
 class EditPlot(UpdateView):
     pass
 
+
 edit_plot = EditPlot.as_view()
+
 
 class DeletePlot(DeleteView):
     pass
 
+
 delete_plot = DeletePlot.as_view()
+
 
 class SearchPlot(ListView):
     pass
+
 
 search_plot = SearchPlot.as_view()
