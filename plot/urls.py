9edf13244a3e0a1c467effd99849ff248d4d7db5
caffeine <eniehack@outlook.jp>
2018-11-28 13:34:50 +0900

    renamed url.py to urls.py

--- plot/urls.py
+++ plot/urls.py
@@ -5,7 +5,7 @@ app_name = 'plot'
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^new/$', views.create_plot),
-    url(r'^(<plot_id>\d+)/', views.view_plot),
-    url(r'^(?P<plot_id>\d+)/edit/$', views.edit_plot),
+    url(r'^(?P<plot_id>[0-9a-z]+)/', views.view_plot),
+    url(r'^(?P<plot_id>[0-9a-z]+)/edit/$', views.edit_plot),
     url(r'^search/$', views.search_plot)
 ]
