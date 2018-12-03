5322da582c6c00dfc0c3f3e47a7dc3f190df38aa
caffeine <eniehack@outlook.jp>
2018-12-03 22:24:13 +0900

    Rewrited url.py for easy viewing.

--- plot/urls.py
+++ plot/urls.py
@@ -1,12 +1,15 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
 
 app_name = 'plot'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^new/$', views.create_plot, name='create'),
-    url(r'^search/$', views.search_plot, name='search'),
-    url(r'^(?P<pk>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/$', views.view_plot, name='details'),
-    url(r'^(?P<pk>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/edit/$', views.edit_plot, name='edit'),
-    url(r'^(?P<pk>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/delete/$', views.delete_plot, name='delete'),
+    url(r'^new/?$', views.create_plot, name='create'),
+    url(r'^search/?$', views.search_plot, name='search'),
+    url(r'^(?P<pk>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/?',
+        include([
+            url(r'^$', views.view_plot, name='details'),
+            url(r'^edit/?$', views.edit_plot, name='edit'),
+            url(r'^delete/?$', views.delete_plot, name='delete'),
+        ])),
 ]
