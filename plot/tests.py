87ea0eacba29aa7aa4c5e408f8334aeb5b46f481
caffeine <eniehack@outlook.jp>
2018-12-05 16:17:11 +0900

    wrote test.

--- plot/tests.py
+++ plot/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from accounts.models import User
 
-# Create your tests here.
+
+class PlotViewTests(TestCase):
+
+    def test_get_index(self):
+        response = self.client.get('/plots/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_new_authenticated(self):
+        self.client.force_login(User.objects.create_user('tester'))
+        response = self.client.post('/plots/new', {'title': 'test', 'content': 'test'})
+        self.assertEqual(response.status_code, 200)
