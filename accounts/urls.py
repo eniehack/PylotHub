0f1dfcebd71e69a972fa32b74f97cdb346c854f6
caffeine <eniehack@outlook.jp>
2018-12-18 22:44:34 +0900

    テンプレートのカスタマイズをするにあたって、オレオレログイン、ログアウト、サインアップページを削除

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,6 @@
-from django.conf.urls import url
-from . import views
+from django.conf.urls import url, include
 
-app_name = 'account'
+app_name = 'accounts'
+
 urlpatterns = [
-    url(r'signin/', views.signin_view, name='signin'),
-    url(r'signup/', views.signup_view, name='signup'),
-    url(r'signout/', views.signout_view, name='signout'),
-]
+] + url(r'^', include('allauth.urls'))
