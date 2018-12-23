60f6f47978e6ef2500ad551afdc2d3ef846348f9
caffeine <eniehack@outlook.jp>
2018-12-23 21:43:53 +0900

    動くかはわからんけどCURDのテストを書いた

--- plot/tests.py
+++ plot/tests.py
@@ -1,5 +1,6 @@
 from django.test import LiveServerTestCase
-from selenium.webdriver.firefox.webdriver import WebDriver 
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.support.ui import Select
 
 
 class PlotViewTests(LiveServerTestCase):
@@ -19,10 +20,43 @@ class PlotViewTests(LiveServerTestCase):
         self.assertEquals(u'プロットをよもう。かこう。 - Plothub', self.selenium.title)
 
     def test_create(self):
-        pass
+        self.selenium.get('%s%s' % (self.live_server_url, '/plots/new/'))
+        self.selenium.implicitly_wait(10)
+        self.assertEquals(u'プロットの新規作成 - PlotHub', self.selenium.title)
+
+        title_input = self.selenium.find_element_by_name('title')
+        genre_element = self.selenium.find_element_by_name('genre')
+        genre_select_element = Select(genre_element)
+        content_input = self.selenium.find_element_by_name('content')
+
+        title_input.send_keys('test')
+        genre_select_element.select_by_value('01')
+        content_input.send_keys('これはSeleniumを使ったテストです')
+
+        self.selenium.find_elements_by_class_name('button is-primary').click()
+        self.selenium.implicitly_wait(10)
+        self.assertEquals('have created your new post!', self.selenium.find)
 
     def test_delete(self):
-        pass
+        self.selenium.get('%s%s' % (self.live_server_url, '/plots/search/'))
+        self.selenium.implicitly_wait(10)
+        self.assertEquals(u'プロット検索 - PlotHub', self.selenium.title)
+
+        self.selenium.find_elements_by_class_name('detail_link').click()
+        self.selenium.implicitly_wait(10)
+
+        self.selenium.find_elements_by_class_name('button is-danger').click()
+        self.selenium.implicitly_wait(10)
+        self.assertEquals(u'プロットの削除 - PlotHub', self.selenium.title)
 
     def test_edit(self):
-        pass
+        self.selenium.get('%s%s' % (self.live_server_url, '/plots/search/'))
+        self.selenium.implicitly_wait(10)
+        self.assertEquals(u'プロット検索 - PlotHub', self.selenium.title)
+
+        self.selenium.find_elements_by_class_name('detail_link').click()
+        self.selenium.implicitly_wait(10)
+
+        self.selenium.find_elements_by_class_name('button is-link').click()
+        self.selenium.implicitly_wait(10)
+        self.assertEquals(u'プロットの編集 - PlotHub', self.selenium.title)
